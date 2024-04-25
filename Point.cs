4657e4b17d92d6510a56220c1afcbb8ff0881242
mrmax <mrmax@DESKTOP-789VVGH>
2024-04-25 19:58:12 +0300

    Добавьте файлы проекта.

---
+++ Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Point<T>
+    {
+        // внутренние поля для координат x и y
+        private T x;
+        private T y;
+
+        // конструктор с двумя параметрами для инициализации координат
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // свойства доступа к координатам x и y
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // метод для вывода значений координат
+        public void Print()
+        {
+            Console.WriteLine($"Точка с координатами ({x}, {y})");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // создание объекта поинт с целочисленными координатами.
+            Point<int> intPoint = new Point<int>(3, 4);
+            Console.WriteLine("Целочисленные координаты:");
+            intPoint.Print();
+
+            // создание объекта поинт с вещественными координатами.
+            Point<double> doublePoint = new Point<double>(1.5, 2.5);
+            Console.WriteLine("\nВещественные координаты:");
+            doublePoint.Print();
+
+            // Создание объекта поинт с символьными координатами.
+            Point<char> charPoint = new Point<char>('A', 'B');
+            Console.WriteLine("\nСимвольные координаты:");
+            charPoint.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
+    
+
